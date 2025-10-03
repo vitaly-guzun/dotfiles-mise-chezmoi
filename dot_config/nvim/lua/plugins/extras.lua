9a43cade311b2ff0f0bbe4622468420306305474
Vitaly Guzun <vitaly.guzun@gmail.com>
2025-10-03 20:24:13 +0000

    Add treesitter parsers

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -4,4 +4,25 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
 	{ import = "lazyvim.plugins.extras.lang.python" },
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
 }
